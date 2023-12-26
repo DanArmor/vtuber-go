72e130a3a8b7021feb8c1afc6d99e948f098d51f
DanArmor <39347109+DanArmor@users.noreply.github.com>
2023-12-26 16:13:11 +0300

    fix: abort on token error

--- pkg/controllers/middleware.go
+++ pkg/controllers/middleware.go
@@ -10,10 +10,12 @@ func (s *Service) CheckToken(c *gin.Context) {
 	token := c.Request.Header.Get("vtubergo-token")
 	if token == "" {
 		c.AbortWithStatusJSON(http.StatusBadRequest, "no token in header of the request")
+		return
 	}
 	payload, err := s.TokenMaker.VerifyToken(token)
 	if err != nil {
 		c.AbortWithStatusJSON(http.StatusBadRequest, "can't verify token")
+		return
 	}
 	c.Set("token-payload", *payload)
 	c.Next()
