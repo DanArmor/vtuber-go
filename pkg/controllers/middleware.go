acb5e4b815797511849431e2e88fb8f8132107c3
DanArmor <39347109+DanArmor@users.noreply.github.com>
2023-12-26 17:16:53 +0300

    feat: response format

--- pkg/controllers/middleware.go
+++ pkg/controllers/middleware.go
@@ -3,18 +3,19 @@ package controllers
 import (
 	"net/http"
 
+	"github.com/DanArmor/vtuber-go/pkg/controllers/resp"
 	"github.com/gin-gonic/gin"
 )
 
 func (s *Service) CheckToken(c *gin.Context) {
 	token := c.Request.Header.Get("vtubergo-token")
 	if token == "" {
-		c.AbortWithStatusJSON(http.StatusBadRequest, "no token in header of the request")
+		c.AbortWithStatusJSON(http.StatusBadRequest, resp.HandlerError(resp.ErrCodeCantValidateInitData, "no token in header of the request"))
 		return
 	}
 	payload, err := s.TokenMaker.VerifyToken(token)
 	if err != nil {
-		c.AbortWithStatusJSON(http.StatusBadRequest, "can't verify token")
+		c.AbortWithStatusJSON(http.StatusBadRequest, resp.HandlerError(resp.ErrCodeCantValidateInitData, "can't verify token"))
 		return
 	}
 	c.Set("token-payload", *payload)
